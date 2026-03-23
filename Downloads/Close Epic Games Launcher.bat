7e951edd095b77ff1dda6901ab0b9883894d0649
Quaked <124531365+QuakedK@users.noreply.github.com>
2026-03-23 10:40:12 -0500

    Add files via upload

---
+++ Downloads/Close Epic Games Launcher.bat
@@ -0,0 +1,19 @@
+:: Made by Quaked
+:: TikTok: _Quaked_
+:: Discord: https://discord.gg/8NqDSMzYun
+
+@echo off
+title Close Epic Games Launcher
+
+:: Close Epic Games Launcher.
+cls
+color A
+chcp 65001 >nul 2>&1
+echo ╔═════════════════════════════════╗
+echo ║ ✅ Closing Epic Games Launcher. ║
+echo ╚═════════════════════════════════╝
+taskkill.exe /F /IM "EpicGamesLauncher.exe" >nul 2>&1
+taskkill.exe /F /IM "EpicOnlineServicesUserHelper.exe" >nul 2>&1
+timeout 3 > nul
+exit
+
